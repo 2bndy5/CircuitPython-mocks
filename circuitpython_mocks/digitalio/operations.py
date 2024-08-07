fe280c65849ea521863032bf562186074573926d
Brendan <2bndy5@gmail.com>
2024-08-07 03:01:57 -0700

    [docs] link cross-link SetState/GetState with DigitalInOut class

--- circuitpython_mocks/digitalio/operations.py
+++ circuitpython_mocks/digitalio/operations.py
@@ -9,14 +9,16 @@ class _State:
 
 
 class SetState(_State):
-    """A class to represent setting the state of a Digital output pin."""
+    """A class to represent setting the state of a
+    :py:class:`~circuitpython_mocks.digitalio.DigitalInOut` pin."""
 
     def __repr__(self) -> str:
         return f"<SetState value='{self.state}'>"
 
 
 class GetState(_State):
-    """A class to represent getting the state of a Digital output pin."""
+    """A class to represent getting the state of a
+    :py:class:`~circuitpython_mocks.digitalio.DigitalInOut` pin."""
 
     def __repr__(self) -> str:
         return f"<GetState value='{self.state}'>"
